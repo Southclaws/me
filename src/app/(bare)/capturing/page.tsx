caab4c282a931e0d3db6ca15971cb42c194ec3e1
Barnaby <accounts@southcla.ws>
2024-06-06 20:48:40 +0100

    add a little... spice

--- src/app/(bare)/capturing/page.tsx
+++ src/app/(bare)/capturing/page.tsx
@@ -1,3 +1,4 @@
+import { Camera3D } from "@/app/components/Camera3D";
 import { PhotoStack } from "@/app/components/PhotoStack/PhotoStack";
 import { css } from "@/styled-system/css";
 import { Box, Center, Grid, Square, VStack, styled } from "@/styled-system/jsx";
@@ -30,81 +31,162 @@ const page = css({
   scrollSnapStop: "always",
 });
 
+const backdrop = css({
+  position: "fixed",
+  width: "full",
+  height: "lvh",
+  padding: "md.fluid",
+  bgColor: "black",
+  zIndex: "-1",
+});
+
 export default async function Page() {
   return (
     <Box maxW="screen">
-      <Center className={page} bgColor="custom.aswad-black" overflowX="hidden">
-        <PhotoStack
-          photos={[
-            {
-              src: "/photography/2019-09-21 spherical.jpg",
-              width: 1694,
-              height: 1368,
-              alt: "Taking a photo of myself in a spherical mirror at the Tom Dixon store in Kings Cross Coal Drops Yard.",
-            },
-            {
-              src: "/photography/DSC00771.jpg",
-              width: 2832,
-              height: 2832,
-              alt: "A lens one of the first founders I worked with gave me as a gift!",
-            },
-            {
-              src: "/photography/2020-01-31 sony-luxembourg.jpg",
-              width: 3456,
-              height: 4608,
-              alt: "The Sony overlooking Luxembourg.",
-            },
-            {
-              src: "/photography/2019-10-02 sony-mountain.jpg",
-              width: 3456,
-              height: 3456,
-              alt: "The Sony in Zakopane.",
-            },
-            {
-              src: "/photography/2020-05-17 sony-forest.jpg",
-              width: 3456,
-              height: 4608,
-              alt: "The Sony on a forest floor.",
-            },
-            {
-              src: "/photography/film at kc.jpg",
-              width: 3024,
-              height: 4032,
-              alt: "Praktika BX20 at Kings Cross.",
-            },
-            {
-              src: "/photography/film in snow.jpg",
-              width: 3024,
-              height: 4032,
-              alt: "Praktika BX20 chilling.",
-            },
-            {
-              src: "/photography/IMG_1409.HEIC.jpg",
-              width: 3024,
-              height: 4032,
-              alt: "Sony by the sea.",
-            },
-            {
-              src: "/photography/IMG_2965.HEIC.jpg",
-              width: 3024,
-              height: 4032,
-              alt: "Praktika BX20 somewhere on a rooftop (I forgot where).",
-            },
-          ]}
-        />
+      <Center className={backdrop}>
+        <Camera3D />
+      </Center>
+
+      <Center
+        className={page}
+        bgColor="black/40"
+        backdropBlur="md"
+        backdropFilter="auto"
+      >
+        <VStack w="full">
+          <styled.h1 fontFamily="futura" color="offwhite">
+            Capturing
+          </styled.h1>
+          <styled.h2 fontFamily="futura" color="offwhite" fontSize="lg">
+            the mosaic of life
+          </styled.h2>
+        </VStack>
+      </Center>
+
+      <Center className={page}>
+        <VStack w="full" h="full" pt="24" justifyContent="space-between">
+          <styled.h1 fontFamily="futura" color="offwhite">
+            My weapon of choice
+          </styled.h1>
+          <Box
+            color="offwhite"
+            fontSize="lg"
+            textAlign="right"
+            textWrap="balance"
+          >
+            <styled.p fontFamily="futura">
+              a (fairly scratched) Sony α7S II from 2015
+            </styled.p>
+            <styled.p fontFamily="futura">
+              (with lenses: 50mm and 16-35mm)
+            </styled.p>
+          </Box>
+        </VStack>
+      </Center>
+
+      <Center className={page} bgColor="white/0" overflowX="hidden">
+        <VStack gap="4" color="offwhite">
+          <styled.h1 letterSpacing="tight">
+            <styled.span display="inline-block" transform="rotate(7.2deg)">
+              the
+            </styled.span>{" "}
+            <styled.span display="inline-block" transform="rotate(-1.2deg)">
+              gear
+            </styled.span>
+          </styled.h1>
+          <PhotoStack
+            photos={[
+              {
+                src: "/photography/2019-09-21 spherical.jpg",
+                width: 1694,
+                height: 1368,
+                alt: "Taking a photo of myself in a spherical mirror at the Tom Dixon store in Kings Cross Coal Drops Yard.",
+              },
+              {
+                src: "/photography/DSC00771.jpg",
+                width: 2832,
+                height: 2832,
+                alt: "A lens one of the first founders I worked with gave me as a gift!",
+              },
+              {
+                src: "/photography/2020-01-31 sony-luxembourg.jpg",
+                width: 3456,
+                height: 4608,
+                alt: "The Sony overlooking Luxembourg.",
+              },
+              {
+                src: "/photography/2019-10-02 sony-mountain.jpg",
+                width: 3456,
+                height: 3456,
+                alt: "The Sony in Zakopane.",
+              },
+              {
+                src: "/photography/2020-05-17 sony-forest.jpg",
+                width: 3456,
+                height: 4608,
+                alt: "The Sony on a forest floor.",
+              },
+              {
+                src: "/photography/film at kc.jpg",
+                width: 3024,
+                height: 4032,
+                alt: "Praktika BX20 at Kings Cross.",
+              },
+              {
+                src: "/photography/film in snow.jpg",
+                width: 3024,
+                height: 4032,
+                alt: "Praktika BX20 chilling.",
+              },
+              {
+                src: "/photography/IMG_1409.HEIC.jpg",
+                width: 3024,
+                height: 4032,
+                alt: "Sony by the sea.",
+              },
+              {
+                src: "/photography/IMG_2965.HEIC.jpg",
+                width: 3024,
+                height: 4032,
+                alt: "Praktika BX20 somewhere on a rooftop (I forgot where).",
+              },
+            ]}
+          />
+          <h2>and its travels</h2>
+        </VStack>
+      </Center>
+
+      <Center className={page} bgColor="offwhite">
+        <VStack
+          alignItems="start"
+          gap="12"
+          p="8"
+          textWrap="balance"
+          maxW="prose"
+          color="offblack"
+        >
+          <h1>photobooks</h1>
+          <p>
+            below are a small collection of photobooks, each one captures a
+            place or a time meaningful to me.
+          </p>
+          <p>
+            if you have any questions about any of the images, please reach out!
+          </p>
+        </VStack>
       </Center>
 
       <Center className={page} bgColor="custom.100mph">
         <VStack>
-          <Square className={square}>
-            <Image
-              src="/photography/city-of-london/DSC03819.jpg"
-              width="1694"
-              height="1368"
-              alt="If you see this sign roaming the back streets of St. Paul's, drop by for a cuppa."
-            />
-          </Square>
           <Link href="/capturing/city-of-london">
+            <Square className={square}>
+              <Image
+                src="/photography/city-of-london/DSC03819.jpg"
+                width="1694"
+                height="1368"
+                alt="If you see this sign roaming the back streets of St. Paul's, drop by for a cuppa."
+              />
+            </Square>
             <styled.h2 fontFamily="albertus" color="offwhite">
               City of London
             </styled.h2>
---
+++ src/app/components/Camera3D.tsx
@@ -0,0 +1,189 @@
+"use client";
+
+import { Environment, Lightformer } from "@react-three/drei";
+import {
+  Canvas,
+  extend,
+  useFrame,
+  useLoader,
+  useThree,
+} from "@react-three/fiber";
+import {
+  BallCollider,
+  Physics,
+  RigidBody,
+  useRopeJoint,
+  useSphericalJoint,
+} from "@react-three/rapier";
+import { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import { useRef, useState } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+
+extend({ MeshLineGeometry, MeshLineMaterial });
+
+export function Camera3D() {
+  return (
+    <Canvas>
+      <Physics gravity={[0, -9.81, 0]}>
+        <Band />
+      </Physics>
+
+      <Environment background blur={0.75}>
+        <color attach="background" args={["black"]} />
+        <Lightformer
+          intensity={180}
+          color="red"
+          position={[0, 1, 3]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={60}
+          color="blue"
+          position={[-1, 0, 1]}
+          rotation={[0, 0, Math.PI / 3]}
+          scale={[100, 0.1, 1]}
+        />
+        <Lightformer
+          intensity={3}
+          color="white"
+          position={[-10, 0, 14]}
+          rotation={[0, Math.PI / 2, Math.PI / 3]}
+          scale={[100, 10, 1]}
+        />
+      </Environment>
+    </Canvas>
+  );
+}
+
+function Band() {
+  const cameraMesh = useLoader(GLTFLoader, "/sonya7sii.glb");
+
+  // References for the band and the joints
+  const band = useRef<any>(null);
+  const fixed = useRef<any>(null);
+  const j1 = useRef<any>(null);
+  const j2 = useRef<any>(null);
+  const j3 = useRef<any>(null);
+
+  const camera = useRef<any>(null);
+  const vec = new THREE.Vector3();
+  const ang = new THREE.Vector3();
+  const rot = new THREE.Vector3();
+  const dir = new THREE.Vector3();
+  const [dragged, drag] = useState<THREE.Vector3 | undefined>(undefined);
+
+  // Canvas size
+  const { width, height } = useThree((state) => state.size);
+
+  // A Catmull-Rom curve
+  const [curve] = useState(
+    () =>
+      new THREE.CatmullRomCurve3([
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+        new THREE.Vector3(),
+      ])
+  );
+
+  useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 0.5]);
+  useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 0.5]);
+  useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 0.5]);
+
+  useSphericalJoint(j3, camera, [
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
+
+  useFrame((state) => {
+    if (!camera.current) return;
+
+    if (dragged) {
+      vec.set(state.pointer.x, state.pointer.y, 0.5).unproject(state.camera);
+      dir.copy(vec).sub(state.camera.position).normalize();
+      vec.add(dir.multiplyScalar(state.camera.position.length()));
+
+      camera.current.setNextKinematicTranslation({
+        x: vec.x - dragged.x,
+        y: vec.y - dragged.y,
+        z: vec.z - dragged.z,
+      });
+    }
+
+    // Calculate Catmull curve
+    curve.points[0].copy(j3.current.translation());
+    curve.points[1].copy(j2.current.translation());
+    curve.points[2].copy(j1.current.translation());
+    curve.points[3].copy(fixed.current.translation());
+    band.current.geometry.setPoints(curve.getPoints(32));
+
+    // Tilt the camera back towards the screen
+    ang.copy(camera.current.angvel());
+    rot.copy(camera.current.rotation());
+    camera.current.setAngvel({
+      x: ang.x,
+      y: ang.y - rot.y * 0.25,
+      // NOTE: This fixes a weird issue with the colmesh importing offset
+      // resulting in the physics being a bit weird as the centre of gravity is
+      // in the wrong place which makes the camera hang unrealistically, I
+      // couldn't figure out what was causing it so this little nudge in the Z
+      // rot just pushes it to the "correct" rotation visually speaking...
+      z: ang.z + rot.z * 0.25,
+    });
+  });
+
+  const damping = {
+    angularDamping: 0.5,
+    linearDamping: 0.5,
+    canSleep: false,
+  };
+
+  return (
+    <>
+      <RigidBody position={[0, 4, 0]} ref={fixed} type="fixed" />
+      <RigidBody {...damping} position={[0, 0, 0]} ref={j1}>
+        <BallCollider args={[0.1]} />
+      </RigidBody>
+      <RigidBody {...damping} position={[1, 0, 0]} ref={j2}>
+        <BallCollider args={[0.1]} />
+      </RigidBody>
+      <RigidBody {...damping} position={[1.5, 0, 0]} ref={j3}>
+        <BallCollider args={[0.1]} />
+      </RigidBody>
+
+      <mesh ref={band}>
+        {/* @ts-ignore */}
+        <meshLineGeometry />
+        {/* @ts-ignore */}
+        <meshLineMaterial
+          color="#1e1e1e"
+          resolution={[width, height]}
+          lineWidth={0.05}
+        />
+      </mesh>
+
+      <RigidBody
+        {...damping}
+        ref={camera}
+        type={dragged ? "kinematicPosition" : "dynamic"}
+        colliders={undefined}
+      >
+        <mesh
+          scale={[0.02, 0.02, 0.02]}
+          onPointerUp={(e) => drag(undefined)}
+          onPointerDown={(e) =>
+            drag(
+              new THREE.Vector3()
+                .copy(e.point)
+                .sub(vec.copy(camera.current.translation()))
+            )
+          }
+        >
+          <primitive object={cameraMesh.scene} />
+        </mesh>
+      </RigidBody>
+    </>
+  );
+}
